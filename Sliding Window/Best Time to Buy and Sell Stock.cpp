c12068f5a31b52c72e07677b19272dec28549847
Madhu Sudhan <2300031210@kluniversity.in>
2026-03-11 17:00:45 +0530

    Sliding Window Problems

---
+++ Sliding Window/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,15 @@
+// Time Complexity: O(n)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        for(int price : prices){
+            minPrice = min(minPrice, price); // smallest value - to buy
+            int profit = price - minPrice; // profit we make with current selling
+            maxProfit = max(maxProfit, profit); // max profit
+        }
+        return maxProfit;
+    }
+};
